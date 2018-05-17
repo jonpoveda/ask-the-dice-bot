76c61373301bc7a9347a600e9f4720b5bd8ac949
JonatanPoveda <jon.poveda@gmail.com>
2018-05-17 19:01:38 +0200

    feat: allow a parameter to retrieve bot info without running it

--- src/bot.py
+++ src/bot.py
@@ -9,10 +9,15 @@ from telegram.ext import CommandHandler
 from telegram.ext import Updater
 
 
-def run(token: str):
+def run(updater: Updater):
+    # Connect bot
+    updater.start_polling()
+
+
+def configure(token: str):
     # Load secret token
     bot = telegram.Bot(token=token)
-    print(bot.get_me())
+    info = bot.get_me()
 
     # Configure an updater to receive updates
     updater = Updater(token)
@@ -31,19 +36,25 @@ def run(token: str):
     # Add command handlers
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-
-    # Connect bot
-    updater.start_polling()
+    return updater, info
 
 
 def main():
     # Parse arguments as parameters
     parser = argparse.ArgumentParser(description='Dataset generator')
     parser.add_argument('token', type=str, help="Bot's API token")
+    parser.add_argument('-i', '--info',
+                        action='store_true',
+                        help='Do not actually run the bot, just configure and '
+                             'retrieve information')
     arguments = parser.parse_args()
 
     # Run bot
-    run(arguments.token)
+    updater, info = configure(arguments.token)
+    if arguments.info:
+        print(info)
+    else:
+        run(updater)
 
 
 if __name__ == '__main__':
